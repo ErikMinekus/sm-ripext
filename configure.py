77ee08f0297d970e6632aa092528605462dd31a0
Erik Minekus <tsunami@tsunami-productions.nl>
2020-09-06 19:30:00 +0200

    Update to AMBuild 2.2

--- configure.py
+++ configure.py
@@ -4,20 +4,25 @@ from ambuild2 import run
 
 # Simple extensions do not need to modify this file.
 
-builder = run.PrepareBuild(sourcePath = sys.path[0])
+ambuild_version = getattr(run, 'CURRENT_API', '2.1')
+if ambuild_version.startswith('2.1'):
+    sys.stderr.write("AMBuild 2.2 or higher is required; please update\n")
+    sys.exit(1)
 
-builder.options.add_option('--hl2sdk-root', type=str, dest='hl2sdk_root', default=None,
+parser = run.BuildParser(sourcePath=sys.path[0], api='2.2')
+
+parser.options.add_argument('--hl2sdk-root', type=str, dest='hl2sdk_root', default=None,
                        help='Root search folder for HL2SDKs')
-builder.options.add_option('--mms-path', type=str, dest='mms_path', default=None,
+parser.options.add_argument('--mms-path', type=str, dest='mms_path', default=None,
                        help='Path to Metamod:Source')
-builder.options.add_option('--sm-path', type=str, dest='sm_path', default=None,
+parser.options.add_argument('--sm-path', type=str, dest='sm_path', default=None,
                        help='Path to SourceMod')
-builder.options.add_option('--enable-debug', action='store_const', const='1', dest='debug',
+parser.options.add_argument('--enable-debug', action='store_const', const='1', dest='debug',
                        help='Enable debugging symbols')
-builder.options.add_option('--enable-optimize', action='store_const', const='1', dest='opt',
+parser.options.add_argument('--enable-optimize', action='store_const', const='1', dest='opt',
                        help='Enable optimization')
-builder.options.add_option('-s', '--sdks', default='all', dest='sdks',
+parser.options.add_argument('-s', '--sdks', default='all', dest='sdks',
                        help='Build against specified SDKs; valid args are "all", "present", or '
-                            'comma-delimited list of engine names (default: %default)')
+                            'comma-delimited list of engine names')
 
-builder.Configure()
+parser.Configure()
