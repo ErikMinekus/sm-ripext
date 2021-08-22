325444432117b25ae899e86d43a703d1373b28ed
Erik Minekus <tsunami@tsunami-productions.nl>
2021-08-22 10:00:00 +0200

    1.3.1

--- smsdk_config.h
+++ smsdk_config.h
@@ -30,7 +30,7 @@
 /* Basic information exposed publicly */
 #define SMEXT_CONF_NAME			"REST in Pawn"
 #define SMEXT_CONF_DESCRIPTION	"Provides HTTP and JSON natives for plugins"
-#define SMEXT_CONF_VERSION		"1.3.0"
+#define SMEXT_CONF_VERSION		"1.3.1"
 #define SMEXT_CONF_AUTHOR		"Tsunami"
 #define SMEXT_CONF_URL			"http://www.tsunami-productions.nl"
 #define SMEXT_CONF_LOGTAG		"RIPEXT"
